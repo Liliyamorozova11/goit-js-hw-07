cac78456a7022d95e1b3d3243b23de158d6a0c74
liliya <li.morozowa1@gmail.com>
2026-03-29 00:39:16 +0200

    fix task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,15 +1,16 @@
+const changeColorButton = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+const body = document.body;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
-const changeColorButton = document.querySelector(".change-color");
-const colorText = document.querySelector(".color");
-
 changeColorButton.addEventListener("click", () => {
   const randomColor = getRandomHexColor();
 
-  document.body.style.backgroundColor = randomColor;
-  colorText.textContent = randomColor;
+  body.style.backgroundColor = randomColor;
+  colorValue.textContent = randomColor;
 });
